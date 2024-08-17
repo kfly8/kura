27f4e4427bb2156eaa441792905509b4c2783c98
kobaken <kentafly88@gmail.com>
2024-08-17 17:22:15 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+kura - It's new $module
+
+# SYNOPSIS
+
+    use kura;
+
+# DESCRIPTION
+
+kura is ...
+
+# LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kobaken <kentafly88@gmail.com>
---
+++ lib/kura.pm
@@ -0,0 +1,39 @@
+package kura;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+kura - It's new $module
+
+=head1 SYNOPSIS
+
+    use kura;
+
+=head1 DESCRIPTION
+
+kura is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
