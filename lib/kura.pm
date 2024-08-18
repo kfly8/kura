d4044a195b4bed8fd3ce511f57fdb1db3dfcdc41
kobaken <kentafly88@gmail.com>
2024-08-18 09:14:46 +0900

    Hide functions

--- lib/kura.pm
+++ lib/kura.pm
@@ -1,6 +1,7 @@
 package kura;
 use strict;
 use warnings;
+use feature qw(state);
 
 our $VERSION = "0.01";
 
@@ -17,99 +18,94 @@ sub import {
     my $class = shift;
     my ($name, $checker) = @_;
 
+    state $validate_name = sub {
+        my ($name) = @_;
+
+        if (!$name) {
+            return 'name is required';
+        }
+        elsif ($forbidden_kura_name{$name}) {
+            return "'$name' is forbidden.";
+        }
+        return;
+    };
+
+    state $validate_checker = sub {
+        my ($checker) = @_;
+
+        unless (defined $checker) {
+            return 'checker is required';
+        }
+
+        return if blessed($checker) && $checker->can('check');
+
+        my $ref = reftype($checker) // '';
+
+        return if $ref eq 'CODE';
+
+        return 'Not a valid checker';
+    };
+
+    state $checker_to_code = sub {
+        my ($checker) = @_;
+
+        if (reftype($checker) eq 'CODE') {
+            require Type::Tiny;
+            $checker = Type::Tiny->new(
+                constraint => $checker,
+            );
+        }
+
+        sub { $checker };
+    };
+
+    state $install_checker = sub {
+        my ($name, $checker, $caller) = @_;
+
+        if ($caller->can($name)) {
+            return "'$name' is already defined";
+        }
+
+        my $code = $checker_to_code->($checker);
+
+        {
+            no strict "refs";
+            *{"$caller\::$name"} = set_subname( "$caller\::$name", $code);
+            push @{"$caller\::EXPORT_OK"}, $name;
+        }
+
+        return;
+    };
+
+    state $setup_exporter = sub {
+        my ($caller) = @_;
+
+        my $exporter_class = 'Exporter';
+
+        unless ($caller->isa($exporter_class)) {
+            no strict "refs";
+            push @{ "$caller\::ISA" }, $exporter_class;
+            ( my $file = $caller ) =~ s{::}{/}g;
+            $INC{"$file.pm"} ||= __FILE__;
+        }
+
+        return;
+    };
+
     my $err;
 
-    $err = $class->_validate_name($name);
+    $err = $validate_name->($name);
     croak $err if $err;
 
-    $err = $class->_validate_checker($checker);
+    $err = $validate_checker->($checker);
     croak $err if $err;
 
     my $caller = caller;
-    $err = $class->_install_checker($name, $checker, $caller);
+    $err = $install_checker->($name, $checker, $caller);
     croak $err if $err;
 
-    $err = $class->_setup_exporter($caller);
+    $err = $setup_exporter->($caller);
     croak $err if $err;
-}
-
-sub _validate_name {
-    my ($class, $name) = @_;
-
-    if (!$name) {
-        return 'name is required';
-    }
-    elsif ($forbidden_kura_name{$name}) {
-        return "'$name' is forbidden.";
-    }
-
-    return;
-}
-
-sub _validate_checker {
-    my ($class, $checker) = @_;
-
-    unless (defined $checker) {
-        return 'checker is required';
-    }
-
-    return if blessed($checker) && $checker->can('check');
-
-    my $ref = reftype($checker) // '';
-
-    return if $ref eq 'CODE';
-
-    return 'Not a valid checker';
-}
-
-sub _checker_to_code {
-    my ($class, $checker) = @_;
-
-    if (reftype($checker) eq 'CODE') {
-        require Type::Tiny;
-        $checker = Type::Tiny->new(
-            constraint => $checker,
-        );
-    }
-
-    sub { $checker };
-}
-
-sub _install_checker {
-    my ($class, $name, $checker, $caller) = @_;
-
-    if ($caller->can($name)) {
-        return "'$name' is already defined";
-    }
-
-    my $code = $class->_checker_to_code($checker);
-
-    {
-        no strict "refs";
-        *{"$caller\::$name"} = set_subname( "$caller\::$name", $code);
-        push @{"$caller\::EXPORT_OK"}, $name;
-    }
-
-    return;
-}
-
-sub _exporter_class {
-    'Exporter';
-}
-
-sub _setup_exporter {
-    my ($class, $caller) = @_;
-
-    my $exporter_class = $class->_exporter_class;
-
-    unless ($caller->isa($exporter_class)) {
-        no strict "refs";
-        push @{ "$caller\::ISA" }, $exporter_class;
-        ( my $file = $caller ) =~ s{::}{/}g;
-        $INC{"$file.pm"} ||= __FILE__;
-    }
-
-    return;
 }
 
 1;
